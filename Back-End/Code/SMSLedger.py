fc40dfa66c9d28ce56be15ef6c6c75c200d5b551
ddr2108 <deepdattaroy8888@gmail.com>
2013-12-20 16:13:02 -0500

    Processing new messages and sending SMS

--- Back-End/Code/SMSLedger.py
+++ Back-End/Code/SMSLedger.py
@@ -1,48 +1,95 @@
-from googlevoice import Voice
 import sys
+import datetime
+import time
+from googlevoice import Voice
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
-import datetime
 
-prevTransaction = -1;
-source = "Deep Datta Roy:"
-#client = MongoClient('localhost', 27017)
-#db = client.test_database
-#collection = db.test_collection
-#collection.insert(({     'first_name': 'John',     'last_name': 'Doe',     'dob': {         'month': 5,         'day': 14,         'year': 1984     } })
-#mongo_collection.find({     'last_name': 'Doe' })
-voice=Voice()
-voice.login("deepdattaroy8888@gmail.com","5891Deep")
+###############################################################################
+########################Initial Setup##########################################
+source = "Deep Datta Roy:"      #holds the number which is the source
+number = "4045148059"           #the actual number
+email = "deepdattaroy8888@gmail.com"    #login info
+pwd = "5891Deep"
+###############################################################################
+###############################################################################
 
+#Global Variables
+prevTransaction = -1           #holds the number of messages earlier
+voice = -1                     #holds the Google Voice Interface
+
+#main function
+def main():
+    global voice
+
+    #Google Voice Interface
+    voice = Voice()
+    voice.login(email,pwd)              #Login
+    while True:
+        voice.sms()                     #Get SMS Data  
+        messages = extractSMS(voice.sms.html)      #Get messages
+        processMessages(messages)                   #Process Recieved Messages
+        time.sleep(30)
+
+#processes the recieved messages
+def processMessages(messages):
+    #Break each message into words
+    for message in messages:
+        messagePieces = message.split()
+        #if 3 words, do more processing
+        if (len(messagePieces)==4 and messagePieces[0].lower() =="ledger"):
+            insertDB(messagePieces)
+            sendSMS("Transaction Added")
+        elif (len(messagePieces)==2 and messagePieces[0].lower() =="ledger"):
+            #data = requestDB(messagePieces)
+            sendSMS("asd")
+
+#insert new info into ledger
+def insertDB(messagePieces):
+    #client = MongoClient('localhost', 27017)
+    #db = client.test_database
+    #collection = db.test_collection
+    #collection.insert(({     'first_name': 'John',     'last_name': 'Doe',     'dob': {         'month': 5,         'day': 14,         'year': 1984     } })
+    #mongo_collection.find({     'last_name': 'Doe' })
+    a = 1
+
+#get data from ledger to send as SMS
+def requestDB(messagePieces):
+    a = 1
+
+#send requested data via SMS
+def sendSMS(data):
+    voice.send_sms(number, data)
 
 #process the SMS HTML file
-def extractsms(htmlsms) :
+def extractSMS(HTMLSMS):
     global prevTransaction
     messagesValid = []                      #array to hold the messages
+    messagesNew = []
 
-    tree = BeautifulSoup(htmlsms)           #convert to beautiful soup object
+    tree = BeautifulSoup(HTMLSMS)           #convert to beautiful soup object
     
     messages = tree.findAll("div","gc-message-sms-row")     #find all texts and pull data
     for message in messages:
-        #get time and if never run before set newest message as the prevTransaction
-        #timeMessage = message.find("span","gc-message-sms-time").get_text()
-        #print(messages)
-        #print(asd)
-        #if prevTransaction == 0:
-        #    prevTransaction = timeMessage
-        #    break
-        #continue getting new messages till reach previous transaction
-        #if timeMessage == prevTransaction:
-        #    break
+        #Seperate messages from correct user
         fromMessage = message.find("span","gc-message-sms-from").get_text()
         if fromMessage.find(source)>=0:
-            print(fromMessage.find(source))
-        textMessage = message.find("span","gc-message-sms-text").get_text()
-        
-    #msgitems.append(msgitems)					# add msg dictionary to list
-    return messagesValid
+            messagesValid.append(message)
+
+    #if this is first go through, set value
+    if prevTransaction == -1:
+        prevTransaction = len(messagesValid)
+        return messagesNew;
     
-while True:
-    voice.sms()
-    extractsms(voice.sms.html)
-    break
+    #new text has arrived
+    if prevTransaction != len(messagesValid):
+        #get the new messages
+        for x in range(prevTransaction, len(messagesValid)):
+            textMessage = messagesValid[x].find("span","gc-message-sms-text").get_text()
+            messagesNew.append(textMessage);
+        prevTransaction = len(messagesValid)        #set new value for messages
+
+    return messagesNew
+
+if __name__=="__main__":
+    main()
